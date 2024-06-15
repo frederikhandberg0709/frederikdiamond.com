22fccc4e1d88723d90e48a02ca7e5444e7081fa5
FREDERIK DIAMOND <github.cringing613@passmail.net>
2024-06-15 19:00:45 +0200

    BlogSection link now working.

--- src/app/components/sections/BlogSection.tsx
+++ src/app/components/sections/BlogSection.tsx
@@ -10,7 +10,7 @@ export default function BlogSection() {
 
       <div className="sticky flex justify-center items-end z-10 bottom-0 h-[150px] w-full bg-gradient-to-b from-transparent to-black/90">
         <Link
-          href="/blog"
+          href="/blog/timeline"
           className="mb-[30px] hover:mb-[35px] text-[15px] font-semibold px-[20px] py-[12px] rounded-full bg-gradient-to-b from-[#0EBEFE] to-[#312FAD] hover:shadow-[0px_5px_30px_0px_#312FAD] transition-all duration-150 ease-in-out"
         >
           Visit my blog
