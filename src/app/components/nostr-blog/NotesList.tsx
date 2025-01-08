a4a0d58b5aa4c1293fa482157808c103e2871c2b
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-01-08 18:10:00 +0100

    Multiple images for the same post now showing

--- src/app/components/nostr-blog/NotesList.tsx
+++ src/app/components/nostr-blog/NotesList.tsx
@@ -38,37 +38,38 @@ interface ProfileFeedProps {
 }
 
 const ProfileFeed: React.FC<ProfileFeedProps> = ({
-  filterType,
-  activeSection,
-  maxElements,
-}) => {
+                                                   filterType,
+                                                   activeSection,
+                                                   maxElements,
+                                                 }) => {
   const [overlayImage, setOverlayImage] = useState<string | null>(null);
-  const { events } = useNostrEvents({
+  const {events} = useNostrEvents({
     filter: {
       authors: [
         "9c9f81ed795f0f5efa558932824687d84fc7e6a4cfa6db5d6d3b50fcb7ffaec2",
       ],
       since: 0,
       kinds: [1],
+      limit: 10,
     },
   });
 
   const pubkey =
-    "9c9f81ed795f0f5efa558932824687d84fc7e6a4cfa6db5d6d3b50fcb7ffaec2";
-  const { data: userData } = useProfile({
+      "9c9f81ed795f0f5efa558932824687d84fc7e6a4cfa6db5d6d3b50fcb7ffaec2";
+  const {data: userData} = useProfile({
     pubkey,
   });
 
   function convertTimestamp(event: any) {
     var d = new Date(event.created_at * 1000), // Convert the passed timestamp to milliseconds
-      yyyy = d.getFullYear(),
-      mm = ("0" + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
-      dd = ("0" + d.getDate()).slice(-2), // Add leading 0.
-      hh = d.getHours(),
-      h = hh,
-      min = ("0" + d.getMinutes()).slice(-2), // Add leading 0.
-      ampm = "AM",
-      time;
+        yyyy = d.getFullYear(),
+        mm = ("0" + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
+        dd = ("0" + d.getDate()).slice(-2), // Add leading 0.
+        hh = d.getHours(),
+        h = hh,
+        min = ("0" + d.getMinutes()).slice(-2), // Add leading 0.
+        ampm = "AM",
+        time;
 
     if (hh > 12) {
       h = hh - 12;
@@ -81,25 +82,51 @@ const ProfileFeed: React.FC<ProfileFeedProps> = ({
     }
 
     // ie: 2014-03-24, 3:00 PM
-    // time = yyyy + "-" + mm + "-" + dd + ", " + h + ":" + min + " " + ampm;
     time = dd + "-" + mm + "-" + yyyy + ", " + h + ":" + min + " " + ampm;
     return time;
   }
+
+  const processContent = (content: string) => {
+    const mediaUrls = extractMediaUrls(content);
+    const textContent = hideMediaLinks(content);
+    return { mediaUrls, textContent };
+  };
 
   const hideMediaLinks = (content: string) => {
     const regex = /https:\/\/.*\.(jpg|png|mp4|avi|mov|webp)/gi;
     return content.replace(regex, "");
   };
 
+  const extractMediaUrls = (content: string) => {
+    const imageRegex = /https:\/\/.*?\.(jpg|png|webp)/gi;
+    const videoRegex = /https:\/\/.*?\.(mp4|avi|mov)/gi;
+
+    const imageMatches = content.match(imageRegex);
+    const videoMatches = content.match(videoRegex);
+
+    const images = imageMatches ? [...imageMatches] : [];
+    const videos = videoMatches ? [...videoMatches] : [];
+
+    // Debug log
+    console.log('Content:', content);
+    console.log('Found images:', images);
+    console.log('Found videos:', videos);
+
+    return {
+      images,
+      videos
+    };
+  };
+
   const filterEvents = (events: any[]) => {
     return events.filter((event) => {
-      const hasImage = /https:\/\/.*\.(jpg|png)/.test(event.content);
-      const hasVideo = /https:\/\/.*\.(mp4|avi|mov)/.test(event.content);
+      const {images, videos} = extractMediaUrls(event.content);
       const isArticle = /#article/i.test(event.content);
+
       if (filterType == "articles" && isArticle) return true;
-      if (filterType === "image" && hasImage) return true;
-      if (filterType === "video" && hasVideo) return true;
-      if (filterType === "text" && !hasImage && !hasVideo) return true;
+      if (filterType === "image" && images.length > 0) return true;
+      if (filterType === "video" && videos.length > 0) return true;
+      if (filterType === "text" && images.length === 0 && videos.length === 0) return true;
       if (filterType === "all") return true;
       return false;
     });
@@ -121,119 +148,120 @@ const ProfileFeed: React.FC<ProfileFeedProps> = ({
     }
   };
 
-  return (
-    <>
-      <div className="flex flex-col gap-[20px] sm:gap-[10px] items-center">
-        {filterEvents(events)
-          .slice(0, maxElements)
-          .map((event) => (
-            <p
-              key={event.id}
-              className="flex flex-col gap-[20px] sm:gap-[10px] w-screen items-center sm:flex-none"
-            >
-              <div className="flex flex-col gap-[10px] sm:w-[600px] w-[90%] sm:border border-gray-900 hover:border-gray-800 transition duration-200 bg-black sm:p-[15px] sm:rounded-[15px]">
-                {/* Author */}
-                <div className="flex items-center justify-between">
-                  <Link
-                    href="https://primal.net/p/npub1nj0crmtetu84a7j43yegy358mp8u0e4ye7ndkhtd8dg0edll4mpqn52gqz"
-                    className="flex gap-[10px] group"
-                  >
-                    {/* Profile picture */}
-                    <img
-                      src={userData?.picture}
-                      alt="Frederik Diamond"
-                      width={40}
-                      height={40}
-                      className="rounded-full h-[40px] w-[40px] object-cover"
-                    />
-                    <div className="flex flex-col gap-[1px]">
-                      {/* Profile name */}
-                      <p className="font-bold text-[13px] group-hover:text-blue-500">
-                        {userData?.display_name}
-                      </p>
-                      {/* Username */}
-                      <p className="text-[13px] text-gray-500">
-                        @{userData?.name}
-                      </p>
-                    </div>
-                  </Link>
-                  <div className="flex items-center gap-[15px]">
-                    {/* Time ago posted */}
-                    <p className="text-[15px] text-right text-textGray">
-                      {/* {event.created_at * 1000} */}
-                      {convertTimestamp(event)}
-                    </p>
-                    <PostDropdownMenu />
-                  </div>
-                </div>
-                {/* Text content */}
-                <p className="text-[15px] leading-normal overflow-x-hidden">
-                  {/* {hideMediaLinks(event.content)} */}
-                  {styleHashtags(hideMediaLinks(event.content))}
-                </p>
-                {/* Image */}
-                {(() => {
-                  const regex = /https:\/\/.*\.(jpg|png|webp)/;
-                  const match = event.content.match(regex);
-                  return match ? (
-                    <img
-                      src={match[0]}
-                      alt="This is an image uploaded by FREDERIK DIAMOND's profile."
-                      onClick={() => handleImageClick(match[0])}
-                      className="rounded-[10px]"
-                    />
-                  ) : null;
-                })()}
-                {/* Video */}
-                {(() => {
-                  const regex = /https:\/\/.*\.(mp4|avi|mov)/;
-                  const match = event.content.match(regex);
-                  return match ? (
-                    <video
-                      src={match[0]}
-                      className="rounded-[10px]"
-                      width="100%"
-                      controls
-                      autoPlay
-                      muted
-                    />
-                  ) : null;
-                })()}
-                {/* Reaction buttons */}
-                {/* <PostActionButton
-                likeCount={event.like_count}
-                commentCount={event.comment_count}
-                shareCount={event.share_count}
-                zapCount={event.zap_count}
-              /> */}
-              </div>
-              <div className="h-[1px] w-full sm:h-0 bg-white/10"></div>
-            </p>
-          ))}
-      </div>
+  const renderMedia = ({ images, videos }: { images: string[], videos: string[] }) => {
+    console.log('Rendering media with:', { images, videos });
 
-      {/* Image Overlay */}
-      {overlayImage && (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-          onClick={handleOverlayClick}
-        >
-          <div className="relative max-w-full max-h-full overflow-hidden">
-            <button
-              className="absolute top-0 right-0 m-4 font-semibold text-white bg-black/50 hover:bg-black/95 rounded-full px-2 py-1"
-              onClick={closeOverlay}
-            >
-              Close
-            </button>
-            <img
-              src={overlayImage}
-              alt="Fullscreen"
-              className="max-h-[90vh] max-w-[90vw] rounded-lg"
-            />
-          </div>
+    if (!images?.length && !videos?.length) {
+      return null;
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+          {images.map((imgUrl, index) => (
+              <img
+                  key={`img-${index}`}
+                  src={imgUrl}
+                  alt={`Media content ${index + 1}`}
+                  onClick={() => handleImageClick(imgUrl)}
+                  className="rounded-[10px] w-full"
+              />
+          ))}
+          {videos.map((videoUrl, index) => (
+              <video
+                  key={`video-${index}`}
+                  src={videoUrl}
+                  className="rounded-[10px] w-full"
+                  controls
+                  autoPlay
+                  muted
+              />
+          ))}
         </div>
-      )}
-    </>
+    );
+  };
+
+  return (
+      <>
+        <div className="flex flex-col gap-[20px] sm:gap-[10px] items-center">
+          {filterEvents(events)
+              .slice(0, maxElements)
+              .map((event) => {
+                const { mediaUrls, textContent } = processContent(event.content);
+                return (
+                  <div
+                      key={event.id}
+                      className="flex flex-col gap-[20px] sm:gap-[10px] w-screen items-center sm:flex-none"
+                  >
+                    <div
+                        className="flex flex-col gap-[10px] sm:w-[600px] w-[90%] sm:border border-gray-900 hover:border-gray-800 transition duration-200 bg-black sm:p-[15px] sm:rounded-[15px]">
+                      {/* Author */}
+                      <div className="flex items-center justify-between">
+                        <Link
+                            href="https://primal.net/p/npub1nj0crmtetu84a7j43yegy358mp8u0e4ye7ndkhtd8dg0edll4mpqn52gqz"
+                            className="flex gap-[10px] group"
+                        >
+                          {/* Profile picture */}
+                          <img
+                              src={userData?.picture}
+                              alt="Frederik Diamond"
+                              width={40}
+                              height={40}
+                              className="rounded-full h-[40px] w-[40px] object-cover"
+                          />
+                          <div className="flex flex-col gap-[1px]">
+                            {/* Profile name */}
+                            <p className="font-bold text-[13px] group-hover:text-blue-500">
+                              {userData?.display_name}
+                            </p>
+                            {/* Username */}
+                            <p className="text-[13px] text-gray-500">
+                              @{userData?.name}
+                            </p>
+                          </div>
+                        </Link>
+                        <div className="flex items-center gap-[15px]">
+                          {/* Time ago posted */}
+                          <p className="text-[15px] text-right text-textGray">
+                            {/* {event.created_at * 1000} */}
+                            {convertTimestamp(event)}
+                          </p>
+                          <PostDropdownMenu/>
+                        </div>
+                      </div>
+                      {/* Text content */}
+                      <div className="text-[15px] leading-normal overflow-x-hidden">
+                        {styleHashtags(textContent)}
+                      </div>
+
+                      {renderMedia(mediaUrls)}
+                    </div>
+                    <div className="h-[1px] w-full sm:h-0 bg-white/10"></div>
+                  </div>
+              )})}
+        </div>
+
+        {/* Image Overlay */}
+        {overlayImage && (
+            <div
+                className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+                onClick={handleOverlayClick}
+            >
+              <div className="relative max-w-full max-h-full overflow-hidden">
+                <button
+                    className="absolute top-0 right-0 m-4 font-semibold text-white bg-black/50 hover:bg-black/95 rounded-full px-2 py-1"
+                    onClick={closeOverlay}
+                >
+                  Close
+                </button>
+                <img
+                    src={overlayImage}
+                    alt="Fullscreen"
+                    className="max-h-[90vh] max-w-[90vw] rounded-lg"
+                />
+              </div>
+            </div>
+        )}
+      </>
   );
 };
 
